f28b3920484fd913db47e8df64ff6daea2cf3b0a
Vikas NS <nsvikas1998@gmail.com>
2018-07-09 15:09:49 +0530

    Update A2.py

--- A2.py
+++ A2.py
@@ -1,1 +1,6 @@
+my_age=[2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+no_of_crushes=[0,0,0,0,0,2,-2,4,-4,4,-4,8,-8,4,-2,2,-2,1,-1.5,0,0,0,0,0]
+from matplotlib import pyplot
 
+pyplot.plot(my_age,no_of_crushes)
+pyplot.show()
